4ad09ac8a8c02f34ca69372f90f0e2afdaf7c91d
LanMJ1998 <983345181@qq.com>
2019-07-24 20:32:04 +0800

    finish index,first commit,Pure static

--- demo/index.js
+++ demo/index.js
@@ -1,9 +1,13 @@
+
 /**
  * @format
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './js/App';
 import {name as appName} from './app.json';
+/*import {AppStackNavigator} from './js/AppNavigators'
+import {createAppContainer} from 'react-navigation';
 
+const AppStackNavigatorContainer=createAppContainer(AppStackNavigator);*/
 AppRegistry.registerComponent(appName, () => App);
